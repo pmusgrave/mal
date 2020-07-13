8a8dbd916ce69b816a42d04bea255ab59650ed43
pmusgrave <ps.musgrave@gmail.com>
2020-07-13 17:54:30 -0400

    complete C++ step 0

--- impls/pm-cpp/step0_repl.cpp
+++ impls/pm-cpp/step0_repl.cpp
@@ -1,6 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
 #include <iostream>
+#include <string>
+
+std::string READ(std::string input) {
+  return input;
+}
+
+std::string EVAL(std::string input) {
+  return input;
+}
+
+std::string PRINT(std::string input) {
+  return input;
+}
+
+std::string rep(std::string input) {
+  std::string buffer;
+  buffer = READ(input);
+  buffer = EVAL(input);
+  buffer = PRINT(input);
+  return buffer;
+}
 
 int main(void) {
-  std::cout << "hello, world" << std::endl;
+  std::string input;
+  std::string result;
+  std::cout << "user> ";
+  while (getline(std::cin, input)) {
+    result = rep(input);
+    std::cout << result << std::endl;
+    std::cout << "user> ";
+  }
   return 0;
 }
